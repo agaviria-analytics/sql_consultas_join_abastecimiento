1c3f70db8c13ea6e6a32fffe8e9d1be0da35fc88
Alejandro <agaviria1408@gmail.com>
2025-07-18 16:47:33 -0500

    Se agregan consultas utiles SQL

--- consultas_utiles.sql
+++ consultas_utiles.sql
@@ -15,3 +15,18 @@ ORDER BY nombre_producto;
 
 --Contar cuantos registros hay
 SELECT COUNT(*) FROM productos;
+
+-- Filtrar productos con más de 20 unidades en stock inicial
+SELECT *
+FROM stock_inicial
+WHERE cantidad_inicial >= 20;
+
+-- Ver solo productos que tienen movimientos registrados
+SELECT DISTINCT p.nombre_producto
+FROM productos p
+INNER JOIN movimientos_stock m ON p.id_producto = m.id_producto;
+
+-- Consultar movimientos tipo 'salida' con cantidad mayor a 5
+SELECT *
+FROM movimientos_stock
+WHERE tipo_movimiento = 'salida' AND cantidad > 5;
